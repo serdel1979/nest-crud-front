caa9a8a085f5715d681c8d394260cd4b454f8602
Sergio De Luca <sdlbsso@gmail.com>
2022-05-07 22:12:06 -0300

    creando front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,28 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { ListaProductoComponent } from './producto/lista-producto.component';
+import { NuevoProductoComponent } from './producto/nuevo-producto.component';
+import { EditarProductoComponent } from './producto/editar-producto.component';
+import { DetalleProductoComponent } from './producto/detalle-producto.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaProductoComponent,
+    NuevoProductoComponent,
+    EditarProductoComponent,
+    DetalleProductoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),    
     FormsModule
   ],
   providers: [],
---
+++ src/app/services/service-producto.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Producto } from '../models/producto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceProductoService {
+
+  productoURL = environment.productosUrl;
+
+  constructor(private httpClient: HttpClient) { }
+
+  public lista(): Observable<Producto[]> {
+    return this.httpClient.get<Producto[]>(`${this.productoURL}`);
+  }
+
+  public detail(id: number): Observable<Producto> {
+    return this.httpClient.get<Producto>(`${this.productoURL}/${id}`);
+  }
+
+  
+  public save(producto: Producto): Observable<any> {
+    return this.httpClient.post<any>(`${this.productoURL}`, producto);
+  }
+
+  public update(id: number, producto: Producto): Observable<any> {
+    return this.httpClient.put<any>(`${this.productoURL}/${id}`, producto);
+  }
+
+  public delete(id: number): Observable<any> {
+    return this.httpClient.delete<any>(`${this.productoURL}/${id}`);
+  }
+}
